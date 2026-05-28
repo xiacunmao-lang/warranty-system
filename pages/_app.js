d94f67babd8df7ed984c36b0ad9b62800a8492c5
xiacunmao-lang <xiacunmao@gmail.com>
2026-05-28 16:17:39 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 // pages/_app.js
-import '../styles/globals.css' // <--- 必须引入这一行！
+import '../styles/globals.css' // <--- 必须加上这一行！
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
